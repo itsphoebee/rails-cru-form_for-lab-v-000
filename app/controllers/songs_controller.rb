9d89f34c14a6b93ea812da65de1f26b0b339c995
Phoebe Lau <phoebelau7@gmail.com>
2018-01-27 03:50:12 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,34 @@
 class SongsController < ApplicationController
+  
+  def new
+    @artist = Artist.new
+  end
 
+  def create
+    @artist = Artist.create(strong_params)
+    redirect_to artist_path(@artist)
+  end
 
+  def show
+    @artist = find_id
+  end
 
+  def edit
+    @artist = find_id
+  end
+
+  def update
+    @artist = find_id
+    @artist.update(strong_params)
+    redirect_to artist_path(@artist)
+  end
+
+  private
+  def strong_params
+    params.require(:artist).permit(:name,:bio)
+  end
+
+  def find_id
+    Artist.find(params[:id])
+  end
 end
