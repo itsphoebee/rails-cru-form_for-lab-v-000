413b1b91b067c0d365e61bf5798004869fe1de8d
Phoebe Lau <phoebelau7@gmail.com>
2018-01-27 03:35:28 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,6 +5,7 @@ class ArtistsController < ApplicationController
   end
 
   def create
+    @artist = Artist.new(strong_params)
   end
 
   def show
@@ -15,5 +16,9 @@ class ArtistsController < ApplicationController
 
   def update
   end
+
+  private
+    def strong_params
+      params.require(:artist).permit(:name,:bio)
 
 end
